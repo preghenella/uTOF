aff15c9b357ec3c60338173fef37e60a1d48b433
Roberto Preghenella <preghenella@bo.infn.it>
2017-09-02 16:06:04 +0200

    Compatibility: apprently boost lib is too old on bocluster

--- src/utils/stripWriter.cxx
+++ src/utils/stripWriter.cxx
@@ -13,15 +13,16 @@ main(int argc, char **argv)
   
   namespace po = boost::program_options;
   po::options_description desc("Options");
-  desc.add_options()
-    ("help", "Print help messages")
-    ("input,i", po::value<std::string>()->default_value("uTOFtree.root"), "Input TOFcalibTree file name")
-    ("output,o", po::value<std::string>()->default_value("uTOFstrip.root"), "Output uTOFtree file name")
-    ("strip,s", po::value<int>()->required(), "TOF strip index")
-    ;
-  
+
   /** process arguments **/
   try {
+    desc.add_options()
+      ("help", "Print help messages")
+      ("input,i", po::value<std::string>(&inputFileName)->default_value("uTOFtree.root"), "Input TOFcalibTree file name")
+      ("output,o", po::value<std::string>(&outputFileName)->default_value("uTOFstrip.root"), "Output uTOFtree file name")
+      ("strip,s", po::value<int>(&strip), "TOF strip index")
+      ;
+    
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     
@@ -30,10 +31,11 @@ main(int argc, char **argv)
       std::cout << desc << std::endl;
       return 1;
     }
-    
-    if (vm.count("input"))  inputFileName  = vm["input"].as<std::string>();
-    if (vm.count("output")) outputFileName = vm["output"].as<std::string>();
-    if (vm.count("strip"))  strip          = vm["strip"].as<int>();
+
+    if (!vm.count("strip")) {
+      std::cout << "Strip is required" << std::endl;
+      return 1;
+    }
     
     po::notify(vm);
   }
